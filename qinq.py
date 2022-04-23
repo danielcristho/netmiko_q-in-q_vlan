a42c300536147c07f632d84fc4c8cfe56a6a8e44
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-04-23 19:54:55 +0900

    update

--- qinq.py
+++ qinq.py
@@ -6,7 +6,7 @@ def read_inventory(yaml_file):
     with open(yaml_file) as f:
         inventory = f.read()
 
-    inventory_dict = yaml.load(inventory)
+    inventory_dict = yaml.safe_load(inventory)
     return inventory_dict
 
 def device_connection(router_ip):
@@ -20,11 +20,11 @@ def device_connection(router_ip):
     conn = ConnectHandler(**device)
     return conn
 
-def conf_int(conn,conf_int):
-    vlanInterface1 = conf_int['interface1']
-    interface = conf_int['interface']
+def conf_int(conn,int_config):
+    vlanInterface = int_config['interface']
+    interface = int_config['interface']
     list_conf = ['interface vlan add name=vlan10 use-service-tag=yes', 
-                '{}'.format(vlanInterface1), '{}'.format(interface)]
+                '{}'.format(vlanInterface), '{}'.format(interface)]
 
     print (conn.send_config_set(list_conf))
 
@@ -34,4 +34,30 @@ def conf_ip(conn, ip_config):
     list_conf = ['ip address add interface= {}'.format(interface),
                 '{}'.format(ip_addr)]
 
-    print (conn.send_config_set(list_conf))            
+    print (conn.send_config_set(list_conf))
+
+def main():
+    yaml_file = 'inventory.yaml'
+    inventory_dict = read_inventory(yaml_file)
+    pprint(inventory_dict)
+
+    for router in inventory_dict['QINQ']:
+
+        router_ip = router['host']
+        print("--------------------------------")
+        print("Configuring {}".format(router_ip))
+        print("--------------------------------")
+
+        conn = device_connection(router_ip)
+
+        #config ip address
+        int_config = router['int_conf']
+        for conf in int_config:
+            conf_ip(conn, conf)
+
+        #config vlan interface
+        vlan_config = router['vlan_conf']
+        for config in vlan_config:
+            conf_int(conn, config)
+
+main()
