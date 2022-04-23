c84c0644185f31a51301a8c0d73b9ccbd352d561
danielcristho <pepuhodaniel93.learn@gmail.com>
2022-04-23 12:19:49 +0900

    update

--- qinq.py
+++ qinq.py
@@ -1,31 +1,21 @@
-from netmiko import ConnectHandler, NetMikoTimeoutException, NetMikoAuthenticationException
-from datetime import datetime
 import yaml
-import time
+from pprint import pprint
+from netmiko import ConnectHandler
 
-if __name__ == '__main__':
-    with open('inventory.yaml') as f:
-        inventory = yaml.safe_load(f.read())
+def read_inventory(yaml_file):
+    with open(yaml_file) as f:
+        inventory = f.read()
 
-    credentials = inventory['vars']
-    devices = len(inventory['hosts'])
-    
+    inventory_dict = yaml.load(inventory)
+    return inventory_dict
 
-#Initialize attributes for establishing connection to target device.
-def send_command(dev: dict, cmd: str) -> str:
-    hostname = device['hostname']
-    del dev['hostname']
+def device_connection(router_ip):
+    device = {
+        "device_type" : "mikrotik_routeros",
+        "ip" : router_ip,
+        "username" : "admin",
+        "password" : "admin123"
+    }
 
-    try:
-        with ConnectHandler(**dev) as conn:
-            conn.enable()
-            output = conn.send_command(cmd)
-        output = output.strip()
-    except NetMikoTimeoutException:
-        print("Connection time out")
-    except NetMikoAuthenticationException:
-        print("Authentication  failed")
-
-    dev['hostname'] = hostname
-
-    return output                
+    conn = ConnectHandler(**device)
+    return conn
